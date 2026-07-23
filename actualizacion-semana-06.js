82d585194b11dea8e452808f7aada3814ac48ccd
crebe-ucayali <berm_km@hotmail.com>
2026-07-22 21:07:26 -0500

    Incorporar diapositivas de la Semana 6

--- actualizacion-semana-06.js
+++ actualizacion-semana-06.js
@@ -6,6 +6,7 @@
   const sesion = capacitaciones.find(item => item.fecha === 'Viernes 17 de julio');
   if (!sesion || !sesion.recursos) return;
 
+  sesion.recursos.pdfDrive = 'https://drive.google.com/file/d/1xsVKoCDtn65Wk2QGeXzySannXM8xFwD4/view';
   sesion.recursos.video = 'https://drive.google.com/file/d/1cNgYn7TZ6nb4z_ZWcD_xk1v7LyenRk17/preview';
   sesion.recursos.videoDrive = 'https://drive.google.com/file/d/1cNgYn7TZ6nb4z_ZWcD_xk1v7LyenRk17/view?usp=sharing';
 
